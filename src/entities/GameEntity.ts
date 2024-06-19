804f5abf2473859946191102f29d6a58773bf260
vguenoff <vguenoff@gmail.com>
2024-06-19 18:58:58 +0300

    fix: more refactor

---
+++ src/entities/GameEntity.ts
@@ -0,0 +1,23 @@
+import { Mesh, Vector3 } from 'three'
+
+abstract class GameEntity {
+  protected _position: Vector3
+  protected _mesh: Mesh = new Mesh()
+
+  get mesh() {
+    return this._mesh
+  }
+
+  constructor(position: Vector3) {
+    this._position = position
+    this._mesh.position.set(
+      this._position.x,
+      this._position.y,
+      this._position.z,
+    )
+  }
+
+  // methods
+  load = async () => {}
+  update = () => {}
+}
--- src/scene/GameScene.ts
+++ src/scene/GameScene.ts
@@ -20,7 +20,7 @@ export default class GameScene {
   // three.js scene
   private readonly _scene = new Scene()
 
-  constructor() {
+  private constructor() {
     // renderer
     this._renderer = new WebGLRenderer({ alpha: true, antialias: true })
     this._renderer.setPixelRatio(window.devicePixelRatio)
@@ -39,7 +39,7 @@ export default class GameScene {
     window.addEventListener('resize', this.resize)
   }
 
-  public resize = () => {
+  private resize = () => {
     this._width = window.innerWidth
     this._height = window.innerHeight
     this._renderer.setSize(this._width, this._height)
@@ -48,14 +48,16 @@ export default class GameScene {
   }
 
   public load() {
-    const geometry = new BoxGeometry(1, 1, 1)
-    const material = new MeshBasicMaterial({ color: 0x00ff })
-    const cube = new Mesh(geometry, material)
+    const cube = new Mesh(
+      new BoxGeometry(1, 1, 1),
+      new MeshBasicMaterial({ color: 0xffffff }),
+    )
     this._scene.add(cube)
   }
 
   public render = () => {
     requestAnimationFrame(this.render)
+
     this._renderer.render(this._scene, this._camera)
   }
 }
